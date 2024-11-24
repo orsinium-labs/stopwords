1f3525ce89988d4150ca469aea5ac92de114c406
gram <git@orsinium.dev>
2024-11-24 16:04:17 +0100

    load words

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/orsinium-labs/stopwords
 
-go 1.23.0
+go 1.23.1
+
+toolchain go1.23.3
+
+require github.com/derekparker/trie/v3 v3.0.0-20240916174412-4095f8e392f7
--- stopwords.go
+++ stopwords.go
@@ -1,9 +1,51 @@
 package stopwords
 
 import (
+	"bufio"
+	"embed"
 	"iter"
+	"strings"
+	"sync"
 	"unicode"
+
+	"github.com/derekparker/trie/v3"
 )
+
+//go:embed words/*.txt
+var files embed.FS
+
+var dicts = sync.OnceValue(func() map[string]*Stopwords {
+	dicts := make(map[string]*Stopwords)
+	dir, err := files.ReadDir("")
+	if err != nil {
+		panic(err)
+	}
+	for _, entry := range dir {
+		fileName := entry.Name()
+		file, err := files.Open(fileName)
+		if err != nil {
+			panic(err)
+		}
+		scanner := bufio.NewScanner(file)
+		dict := trie.New[struct{}]()
+		for scanner.Scan() {
+			dict.Add(scanner.Text(), struct{}{})
+		}
+		dicts[fileName[:2]] = (*Stopwords)(dict)
+	}
+	files = embed.FS{}
+	return dicts
+})
+
+func init() {
+	go dicts()
+}
+
+type Stopwords trie.Trie[struct{}]
+
+func Get(lang string) *Stopwords {
+	return dicts()[strings.ToLower(lang[:2])]
+}
 
 // A word in a text.
 type Match struct {
@@ -15,11 +57,11 @@ type Match struct {
 	Word string
 }
 
-// Iterate over all stopwords in the text
-func Find(text string) iter.Seq[Match] {
+// Iterate over all stopwords in the text.
+func (s *Stopwords) Find(text string) iter.Seq[Match] {
 	return func(yield func(Match) bool) {
 		for match := range iterWords(text) {
-			if IsStopword(match.Word) {
+			if s.Contains(match.Word) {
 				keepGoing := yield(match)
 				if !keepGoing {
 					break
@@ -30,10 +72,10 @@ func Find(text string) iter.Seq[Match] {
 }
 
 // Iterate over all words in the text except the stopwords.
-func Exclude(text string) iter.Seq[Match] {
+func (s *Stopwords) Exclude(text string) iter.Seq[Match] {
 	return func(yield func(Match) bool) {
 		for match := range iterWords(text) {
-			if !IsStopword(match.Word) {
+			if !s.Contains(match.Word) {
 				keepGoing := yield(match)
 				if !keepGoing {
 					break
@@ -44,7 +86,7 @@ func Exclude(text string) iter.Seq[Match] {
 }
 
 // Check if the given word is a stopword.
-func IsStopword(word string) bool {
+func (s *Stopwords) Contains(word string) bool {
 	panic("todo")
 }
 
